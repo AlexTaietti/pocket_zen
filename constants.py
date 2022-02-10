6c4f6d076b5ec2105170576ec9be6f7a292fc9d3
Alex Taietti <alex@withasap.com>
2022-02-10 14:57:26 +0100

    store every constant in a constants module

---
+++ constants.py
@@ -0,0 +1,24 @@
+# constant strings
+FOREGROUND = 'fore'
+BACKGROUND = 'back'
+
+BLACK = 'black'
+RED = 'red'
+GREEN = 'green'
+YELLOW = 'yellow'
+BLUE = 'blue'
+MAGENTA = 'magenta'
+CYAN = 'cyan'
+WHITE = 'white'
+
+# ansi characters
+ANSI_START = '\u001b['
+ANSI_END = 'm'
+RESET_CHARACTER = '0'
+
+BOLD_CODE = '1'
+UNDERLINE_CODE = '4'
+REVERSED_CODE = '7'
+
+# general
+END_OF_LINE = '\r\n'
