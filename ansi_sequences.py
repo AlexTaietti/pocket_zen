5bea6f8861ac51aa867578198f8679112d0a3a57
Alex Taietti <alex@withasap.com>
2022-02-11 19:12:55 +0100

    refactor ansi_sequences module and move constants

--- ansi_sequences.py
+++ ansi_sequences.py
@@ -1,23 +1,5 @@
 import constants
 
-# color codes
-COLORS = {
-    constants.BLACK: 0,
-    constants.RED: 1,
-    constants.GREEN: 2,
-    constants.YELLOW: 3,
-    constants.BLUE: 4,
-    constants.MAGENTA: 5,
-    constants.CYAN: 6,
-    constants.WHITE: 7
-}
-
-# layers
-LAYERS = {
-    constants.FOREGROUND: 3,
-    constants.BACKGROUND: 4
-}
-
 
 # turn codes into actual ansi sequences
 def wrap_modifier(modifier_code):
@@ -28,67 +10,49 @@ def wrap_modifier(modifier_code):
 # reset decoration sequence
 RESET = wrap_modifier(constants.RESET_CHARACTER)
 
-# font style sequences
-BOLD = wrap_modifier(constants.BOLD_CODE)
-UNDERLINE = wrap_modifier(constants.UNDERLINE_CODE)
-REVERSED = wrap_modifier(constants.REVERSED_CODE)
 
-
-def get_color_code(color):
-    return COLORS[color]
-
-
-def get_layer_code(layer):
-    return LAYERS[layer]
-
-
-# more color data accessors and utils
-def get_layer_color_code(color, layer):
-    color = get_color_code(color)
-    layer = get_layer_code(layer)
-    return f'{layer}{color}'
-
-
-def get_colors():
-    return COLORS.items()
-
-
-def get_color_names():
-    return COLORS.keys()
-
-
-def get_layers():
-    return LAYERS.items()
-
-
-def get_layer_names():
-    return LAYERS.keys()
-
-
+# useful utility used to make strings beautiful
 def apply_modifier(string, modifier):
-    pretty_string = f'{modifier}{string}'
-    return pretty_string
+    if string.endswith(RESET):
+        string = f'{modifier}{string}'
+    else:
+        string = f'{modifier}{string}{RESET}'
+    return string
 
 
-def reset_modifiers(string):
-    new_modified_string = f'{string}{RESET}'
-    return new_modified_string
+# make a list of wrapped ansi text style sequences ready to be prepended to any unicode string
+def make_text_style_sequences():
+    text_style_to_ansi = dict()
+    text_styles = constants.TEXT_STYLES.items()
+    for style, code in text_styles:
+        text_style_to_ansi[style] = wrap_modifier(code)
+    return text_style_to_ansi
 
 
-# make a list of wrapped ansi sequences ready to be prepended to any unicode string
+TEXT_STYLE_SEQUENCES = make_text_style_sequences()
+
+
+# make 2 lists of wrapped ansi color sequences (foreground and background) ready to be prepended to any unicode string
 def make_color_sequences_for_layer(layer):
     color_name_to_ansi = dict()
-    color_names = get_color_names()
+    color_names = constants.COLORS.keys()
     for color_name in color_names:
         layer_color_code = get_layer_color_code(color_name, layer)
         color_name_to_ansi[color_name] = wrap_modifier(layer_color_code)
     return color_name_to_ansi
 
 
+def get_layer_color_code(color, layer):
+    color = constants.COLORS[color]
+    layer = constants.LAYERS[layer]
+    return f'{layer}{color}'
+
+
 FG_COLOR_SEQUENCES = make_color_sequences_for_layer(constants.FOREGROUND)
 BG_COLOR_SEQUENCES = make_color_sequences_for_layer(constants.BACKGROUND)
 
 
+# module's interface
 def get_foreground_color(color):
     return FG_COLOR_SEQUENCES[color]
 
@@ -97,3 +61,5 @@ def get_background_color(color):
     return BG_COLOR_SEQUENCES[color]
 
 
+def get_text_style_code(text_style):
+    return TEXT_STYLE_SEQUENCES[text_style]
--- constants.py
+++ constants.py
@@ -11,14 +11,41 @@ MAGENTA = 'magenta'
 CYAN = 'cyan'
 WHITE = 'white'
 
+BOLD = 'bold'
+UNDERLINE = 'underline'
+REVERSE = 'reverse'
+
 # ansi characters
 ANSI_START = '\u001b['
 ANSI_END = 'm'
 RESET_CHARACTER = '0'
 
-BOLD_CODE = '1'
-UNDERLINE_CODE = '4'
-REVERSED_CODE = '7'
+
+# color codes
+COLORS = {
+    BLACK: 0,
+    RED: 1,
+    GREEN: 2,
+    YELLOW: 3,
+    BLUE: 4,
+    MAGENTA: 5,
+    CYAN: 6,
+    WHITE: 7
+}
+
+# layers
+LAYERS = {
+    FOREGROUND: 3,
+    BACKGROUND: 4
+}
+
+
+# text styles
+TEXT_STYLES = {
+    BOLD: 1,
+    UNDERLINE: 4,
+    REVERSE: 7
+}
 
 # general
 END_OF_LINE = '\r\n'
