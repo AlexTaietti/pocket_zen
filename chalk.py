118cccfbf9f9698588845901ad5974ed9b8f06a7
Alex Taietti <alex@withasap.com>
2022-02-11 19:13:28 +0100

    modify_string now supports text style modifications

--- chalk.py
+++ chalk.py
@@ -1,16 +1,19 @@
-from ansi_sequences import apply_modifier, reset_modifiers, get_foreground_color, get_background_color
+from ansi_sequences import apply_modifier, get_foreground_color, get_background_color, get_text_style_code
 
 
-def color_string(string, text_color, background=None):
-    color_code = get_foreground_color(text_color)
-    pretty_string = apply_modifier(string, color_code)
+def modify_string(string, text_color=None, background=None, text_style=None):
+    if text_color:
+        color_code = get_foreground_color(text_color)
+        string = apply_modifier(string, color_code)
     if background:
         background_color_code = get_background_color(background)
-        pretty_string = apply_modifier(pretty_string, background_color_code)
-    pretty_string = reset_modifiers(pretty_string)
-    return pretty_string
+        string = apply_modifier(string, background_color_code)
+    if text_style:
+        style_code = get_text_style_code(text_style)
+        string = apply_modifier(string, style_code)
+    return string
 
 
 if __name__ == '__main__':
-    s = color_string('=============', 'black', background='blue')
+    s = modify_string('=============', 'black', background='blue')
     print(s)
