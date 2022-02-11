2d7a009b04053baac981040b6047b636feb906b9
Alex Taietti <alex@withasap.com>
2022-02-11 23:18:54 +0100

    extract constants from zen module and tidy up code

--- zen.py
+++ zen.py
@@ -4,60 +4,59 @@ import constants
 from chalk import modify_string
 
 
-def wrap_line(line, text_color='cyan', background_color='black', text_style=None):
-    line = modify_string(line, text_color, background_color, text_style)
+def end_line(line):
     line = f"{line}{constants.END_OF_LINE}"
     return line
 
 
-def repeat_char(char, length):
-    s = ''.join([char for x in range(length)])
+def repeat_string(string, length):
+    s = ''.join([string for x in range(length)])
     return s
 
 
-def repeat_line(line, n_lines):
-    lines = ''.join([line for x in range(n_lines)])
-    return lines
-
-
-def make_padding_line(line_width, frame_y):
-    line = f"{frame_y}{repeat_char(' ', line_width - 2)}{frame_y}"
-    line = wrap_line(line)
+def make_padding(line_width):
+    line = f"{constants.FRAME_Y_CHAR}{repeat_string(' ', line_width - 2)}{constants.FRAME_Y_CHAR}"
+    line = modify_string(line, constants.DEFAULT_TEXT_COLOR, constants.DEFAULT_BACKGROUND_COLOR)
+    line = end_line(line)
     return line
 
 
-def make_frame_horizontal_line(line_width, frame_x):
-    line = f"{repeat_char(frame_x, line_width)}"
-    line = wrap_line(line)
+def make_frame_horizontal_side(line_width):
+    line = f"{repeat_string(constants.FRAME_INNER_X_CHAR, line_width)}"
+    line = modify_string(line, constants.DEFAULT_TEXT_COLOR, constants.DEFAULT_BACKGROUND_COLOR)
+    line = end_line(line)
     return line
 
 
-def make_text_line(line, padding, frame_y):
-    frame_y = modify_string(frame_y, 'cyan', 'black')
-    pad = repeat_char(' ', padding)
-    pad = modify_string(pad, 'cyan', 'black')
-    text = modify_string(line, 'cyan', 'black', 'underline')
-    line = f"{frame_y}{pad}{text}{pad}{frame_y}{constants.END_OF_LINE}"
+def make_text_line(line, padding):
+    pad = repeat_string(' ', padding)
+    left_pad = f"{constants.FRAME_Y_CHAR}{pad}"
+    right_pad = f"{pad}{constants.FRAME_Y_CHAR}"
+    left_pad = modify_string(left_pad, constants.DEFAULT_TEXT_COLOR, constants.DEFAULT_BACKGROUND_COLOR)
+    right_pad = modify_string(right_pad, constants.DEFAULT_TEXT_COLOR, constants.DEFAULT_BACKGROUND_COLOR)
+    text = modify_string(line, constants.DEFAULT_TEXT_COLOR, constants.DEFAULT_BACKGROUND_COLOR, 'underline')
+    line = f"{left_pad}{text}{right_pad}"
+    line = end_line(line)
     return line
 
 
-def frame_zen(zen, frame_x, frame_y, horizontal_padding=4, vertical_padding=1):
+def frame_zen(zen):
     text_width = len(zen)
-    frame_width = text_width + (horizontal_padding * 2) + 2
-    frame_side_x = make_frame_horizontal_line(frame_width, frame_x)
-    padding_line = make_padding_line(frame_width, frame_y)
-    text_line = make_text_line(zen, horizontal_padding, frame_y)
-    y_padding = repeat_line(padding_line, vertical_padding)
+    frame_width = text_width + (constants.HORIZONTAL_PADDING * 2) + 2
+    frame_side_x = make_frame_horizontal_side(frame_width)
+    padding_line = make_padding(frame_width)
+    text_line = make_text_line(zen, constants.HORIZONTAL_PADDING)
+    y_padding = repeat_string(padding_line, constants.VERTICAL_PADDING)
     frame = frame_side_x + y_padding + text_line + y_padding + frame_side_x
     return frame
 
 
-def python_zen(frame_x_char="=", frame_y_char="|"):
+def python_zen():
     with open("zen.txt") as zen:
         commandments = [commandment for commandment in zen]
     nugget_of_wisdom = random.choice(commandments)
     stripped_nugget = nugget_of_wisdom.strip()
-    framed_wisdom = frame_zen(stripped_nugget, frame_x_char, frame_y_char)
+    framed_wisdom = frame_zen(stripped_nugget)
     return framed_wisdom
 
 
